b1cab7d99ac59d6b5aa938b750bdb57fe6d5571a
Filip Pekar <pekarf321@gmail.com>
2025-07-11 21:40:05 +0200

    Format code, make functions,adjust for reusability

--- script.js
+++ script.js
@@ -10,164 +10,244 @@ let pressedDigit = "";
 let num1, num2, isOp;
 let op = "";
 let startCalc = 0;
+let currentDisplay = "";
+let totalDisplay = ""; 
 
 digits.forEach(digit => {
-    digit.addEventListener("click", (e) => {
-        if (isOp) {
-            out.textContent = "";
-            if (op === "=") {
-                outTotal.textContent = "";
-            }
-            dot.disabled = false;
-            dot.removeAttribute("style");
-            isOp = false;
-        }
-        if (pressedDigit === "") {
-            out.textContent = "";
-            out.textContent += e.target.textContent;
-            outTotal.textContent += e.target.textContent;
-            pressedDigit += e.target.textContent;
-            if (outTotal.textContent === "0") {
-                outTotal.textContent = "";
-            }
-        }
-        else {
-            if (pressedDigit === "0" && op != "") {
-                return;
-            }
-            else if (pressedDigit == "0" && e.target.textContent != "0") {
-                pressedDigit = "";
-                out.textContent = e.target.textContent;
-                outTotal.textContent = e.target.textContent;
-                pressedDigit += e.target.textContent;
-            }
-            else if (pressedDigit == "0") {
-                pressedDigit = "";
-            }
-            else {
-                out.textContent += e.target.textContent;
-                outTotal.textContent += e.target.textContent;
-                pressedDigit += e.target.textContent;
-            }
-        }
-        
-    });
+    digit.addEventListener("click", (e) => handleDigits(e.target.textContent));
+});
+
+document.addEventListener("keydown", (e) => {
+    if (e.key >= "0" && e.key <= "9") {
+        handleDigits(e.key);
+    }
 });
 
 operators.forEach(operator => {
-    operator.addEventListener("click", (e) => {
-        isOp = true;
-        if (startCalc === 0) {
-            if (pressedDigit === "") {
-                return;
-            }
-            num1 = Number(pressedDigit);
-            pressedDigit = "";
-            op = e.target.textContent;
-            if (op !== "=") {
-                outTotal.textContent += " " + op + " ";
-            }
-        }
-        else {
-            if (pressedDigit === "") {
-                let tempOp = e.target.textContent;
-                if (tempOp !== "=") {
-                    op = e.target.textContent;
-                    outTotal.textContent = num1 + " " + op + " ";
-                }
-                return op;
-            }
-            num2 = Number(pressedDigit);
-            pressedDigit = "";
-            num1 = checkOperator(num1, num2, op);
-            if (isNaN(num1)) {
-                all.forEach(btn => {
-                    btn.disabled = true;
-                    btn.style.opacity = "0.5";
-                });
-            }
-            op = e.target.textContent;
-            if (op !== "=") {
-                outTotal.textContent = num1 + " " + op + " ";
-            }
-        }
-        startCalc++;
-    });
+    operator.addEventListener("click", (e) => handleOperator(e.target.textContent));
 });
 
-clear.addEventListener("click", () => {
+document.addEventListener("keydown", (e) => {
+    if (e.key === "+" || e.key === "-" || e.key === "*" || e.key === "/" || e.key === "=") {
+        handleOperator(e.key);
+    }
+});
+
+clear.addEventListener("click", resetCalc);
+
+del.addEventListener("click", deleteDigit);
+
+document.addEventListener("keydown", (e) => {
+    if (e.key === "Backspace") {
+        deleteDigit();
+    }
+});
+
+dot.addEventListener("click", (e) => handleDot(e.target.textContent));
+
+document.addEventListener("keydown", (e) => {
+    if (e.key === ".") {
+        handleDot(e.key);
+    }
+});
+
+function handleDigits(digit) {
+    if (isOp) {
+        currentDisplay = "";
+        if (op === "=") {
+            totalDisplay = "";
+        }
+        switchButtons(false, "dot");
+        isOp = false;
+    }
+    if (pressedDigit === "") {
+        currentDisplay = "";
+        currentDisplay += digit;
+        totalDisplay += digit;
+        pressedDigit += digit;
+        if (totalDisplay === "0") {
+            totalDisplay = "";
+        }
+    }
+    else {
+        if (pressedDigit === "0" && op != "") {
+            return;
+        }
+        else if (pressedDigit == "0" && digit != "0") {
+            pressedDigit = "";
+            currentDisplay = digit;
+            totalDisplay = digit;
+            pressedDigit += digit;
+        }
+        else if (pressedDigit == "0") {
+            pressedDigit = "";
+        }
+        else {
+            currentDisplay += digit;
+            totalDisplay += digit;
+            pressedDigit += digit;
+        }
+    }
+    render();
+}
+
+function handleOperator(digit) {
+    isOp = true;
+    if (startCalc === 0) {
+        if (pressedDigit === "") {
+            return;
+        }
+        num1 = Number(pressedDigit);
+        pressedDigit = "";
+        op = digit;
+        if (op !== "=") {
+            totalDisplay += " " + op + " ";
+        }
+    }
+    else {
+        if (pressedDigit === "") {
+            let tempOp = digit;
+            if (tempOp !== "=") {
+                op = digit;
+                totalDisplay = num1 + " " + op + " ";
+            }
+            render();
+            return op;
+        }
+        num2 = Number(pressedDigit);
+        pressedDigit = "";
+        num1 = checkOperator(num1, num2, op);
+        if (isNaN(num1)) {
+            all.forEach(btn => {
+                btn.style.opacity = "0.5";
+            });
+        }
+        op = digit;
+        if (op !== "=") {
+            totalDisplay = num1 + " " + op + " ";
+        }
+    }
+    startCalc++;
+    render();
+}
+
+function handleDot(digit) {
+    if (pressedDigit !== "" || pressedDigit === "0" || op === "") {
+        pressedDigit += digit;
+        switchButtons(true, "dot");
+        currentDisplay += digit;
+        if (totalDisplay.at(-1) === "0") {
+            totalDisplay += digit;
+            render();
+            return;
+        }
+        if (pressedDigit === "." || pressedDigit === "0.") {
+            totalDisplay += "0" + digit;
+        }
+        else {
+            totalDisplay += digit;
+        }
+    }
+    if (pressedDigit.at(0) === ".") {
+        pressedDigit = "0" + pressedDigit;
+        if (totalDisplay.at(0) !== "0" && totalDisplay.at(0) !== "") {
+            totalDisplay = "0" + totalDisplay;
+        }
+        if (currentDisplay.at(0) !== "0") {
+            currentDisplay = "0" + currentDisplay;
+        }
+        
+    }
+    render();
+}
+
+function deleteDigit() {
+    if (pressedDigit !== "") {
+        totalDisplay = totalDisplay.slice(0, totalDisplay.length - 1);
+    }
+    if (!isOp) {
+        pressedDigit = pressedDigit.slice(0, pressedDigit.length - 1);
+        currentDisplay = pressedDigit;
+        if (currentDisplay === "") {
+            currentDisplay = "0";
+        }
+        if (!currentDisplay.includes(".")) {
+            switchButtons(false, "dot");
+        }
+    }
+    render();
+}
+
+function checkOperator(num1, num2, op) {
+    switch (op) {
+        case "+":
+            currentDisplay = addNumbers(num1, num2);
+            totalDisplay = currentDisplay;
+            break;
+        case "-":
+            currentDisplay = subNumbers(num1, num2);
+            totalDisplay = currentDisplay;
+            break;
+        case "x":
+            currentDisplay = mulNumbers(num1, num2);
+            totalDisplay = currentDisplay;
+            break;
+        case "/":
+            currentDisplay = divNumbers(num1, num2);
+            totalDisplay = currentDisplay;
+            break;
+        case "%":
+            currentDisplay = remNumbers(num1, num2);
+            totalDisplay = currentDisplay;
+            break;
+        default:
+            currentDisplay - "ERROR";
+    }
+    render();
+    return Number(currentDisplay);
+}
+
+function resetCalc() {
     pressedDigit = "";
     num1 = 0;
     num2 = 0;
     op = "";
     isOp = false;
     startCalc = 0;
-    out.textContent = "0";
-    outTotal.textContent = "";
+    currentDisplay = "0";
+    totalDisplay = "";
     all.forEach(btn => {
-        btn.disabled = false;
-        btn.removeAttribute("style");
+        switchButtons(false, btn);
     });
-});
+    render();
+}
 
-del.addEventListener("click", () => {
-    if (pressedDigit !== "") {
-        outTotal.textContent = outTotal.textContent.slice(0, outTotal.textContent.length - 1);
-    }
-    if (!isOp) {
-        pressedDigit = pressedDigit.slice(0, pressedDigit.length - 1);
-        out.textContent = pressedDigit;
-        if (out.textContent === "") {
-            out.textContent = "0";
-        }
-        if (!out.textContent.includes(".")) {
-            dot.removeAttribute("style");
-            dot.disabled = false;
-        }
-    }
-});
+function render() {
+    out.textContent = currentDisplay;
+    outTotal.textContent = totalDisplay;
+}
 
-dot.addEventListener("click", (e) => {
-    if (pressedDigit !== "" || pressedDigit === "0" || op === "") {
-        pressedDigit += e.target.textContent;
+
+function switchButtons(nextState, el) {
+    if (nextState && el === "dot") {
         dot.disabled = true;
         dot.style.opacity = 0.5;
-        out.textContent += e.target.textContent;
-        outTotal.textContent += e.target.textContent;
+        return;
     }
-    if (pressedDigit.at(0) === ".") {
-        pressedDigit = "0" + pressedDigit;
-        outTotal.textContent = "0" + outTotal.textContent;
+    else if (!nextState && el === "dot") {
+        dot.disabled = false;
+        dot.removeAttribute("style");
+        return;
     }
-});
-
-function checkOperator(num1, num2, op) {
-    switch (op) {
-        case "+":
-            out.textContent = addNumbers(num1, num2);
-            outTotal.textContent = out.textContent;
-            break;
-        case "-":
-            out.textContent = subNumbers(num1, num2);
-            outTotal.textContent = out.textContent;
-            break;
-        case "x":
-            out.textContent = mulNumbers(num1, num2);
-            outTotal.textContent = out.textContent;
-            break;
-        case "/":
-            out.textContent = divNumbers(num1, num2);
-            outTotal.textContent = out.textContent;
-            break;
-        case "%":
-            out.textContent = remNumbers(num1, num2);
-            outTotal.textContent = out.textContent;
-            break;
-        default:
-            out.textContent - "ERROR";
+    else if (nextState) {
+        el.disabled = true;
+        el.style.opacity = 0.5;
+        return;
     }
-    
-    return Number(out.textContent);
+    else {
+        el.disabled = false;
+        el.removeAttribute("style");
+    }
 }
 
 function addNumbers(num1, num2) {
